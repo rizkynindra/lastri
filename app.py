5e250635ae5e00cf9ba2f476e6e10756806635de
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2026-06-18 11:46:31 +0700

    change port

--- app.py
+++ app.py
@@ -217,6 +217,6 @@ def health():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 4848))
     # Enable debugging during development
     app.run(host="0.0.0.0", port=port, debug=True)
